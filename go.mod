2d3a91ee2d14d630831c146958e634b8718324a7
Luca Greggio <l.greggio@reply.it>
2023-09-18 14:51:04 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cedrata/go-rest
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type testHandler struct{}
+
+func (testHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("hello"))
+	time.Sleep(7 * time.Second)
+}
+
+func main() {
+	log.Println("server starting")
+
+	mux := http.NewServeMux()
+	mux.Handle("/test", &testHandler{})
+
+	srv := &http.Server{
+		Addr:    ":8000",
+		Handler: mux,
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	log.Println("server started")
+
+	<-done
+	log.Println("server stopped")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		cancel()
+	}()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("server shutdown failed:%+v", err)
+	}
+
+	log.Print("server exited properly")
+}
